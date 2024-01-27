c2a79331761ce0a686ed9eaa172740136a4cc19b
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-01-27 15:04:31 +0100

    added inverted insertion sort

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -20,3 +20,26 @@ InsertionSort(Unsorted);
 for (int i = 0; i < Unsorted.Length; i++) {
     Console.Write($"{Unsorted[i]} ");
 }
+Console.WriteLine();
+InsertionSortReverse(Unsorted);
+
+for (int i = 0; i < Unsorted.Length; i++) {
+    Console.Write($"{Unsorted[i]} ");
+}
+//---------------------------------------
+
+void InsertionSortReverse(int[] Array) {
+
+    for (int j = 1; j < Array.Length; j++) {
+        int key = Array[j];     //current number
+        int i = j + 1;          //precedent number
+
+        //insert current(key) in the right position of sorted Array[1.. j - 1]
+        while (i >= 0 && Array[i] > key) {   //i is valid index and precedent bigger than current(key)
+            Array[i + 1] = Array[i];         //precedent and current value swap
+            i = i - 1;                       //scan the array backwards to find place to current
+        }
+        
+        Array[i + 1] = key;              //set the current(key) to the place next to current
+    }
+}
