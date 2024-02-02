5923d4dc2a9b848aad54e0e46a2a36baee44eb72
Jojo <giorgio.bruschi.ext@luxottica.com>
2024-02-02 16:52:43 +0100

    added Horner's rule pseudocode

---
+++ Horner's Rule/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+
+
+Write pseudocode to implement the naive polynomial-evaluation algorithm that computes each term of the polynomial from scratch. 
+What is the running time of this algorithm? How does it compare to Horner’s rule?
+
+y = 0
+for i = n downto 0
+    y = a[i] + x * y
+
+Naive-Horner (A, n)
+y = 0
+for i = 1 to A.length
+    m = 0
+    for j = 0 to i - 1
+        m = m * n
+    y = y + A[i] * m
+
+*/
